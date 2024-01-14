0938461436990c3e485aeab4bb22c554fbb7cde2
kariamburi <paul.irungu@gmail.com>
2024-01-14 12:26:27 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+  publicRoutes: ['/api/webhook/clerk'],
+  ignoredRoutes: ['/api/webhook/clerk']
 });
  
 export const config = {
