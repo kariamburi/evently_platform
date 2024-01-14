92def451c05c5f704d866a22aee973876de905b1
kariamburi <paul.irungu@gmail.com>
2024-01-14 13:39:28 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({ 
   ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)"],  
   publicRoutes: ["/api/webhooks/clerk"],
+    debug: true
 });
 
 export const config = {
