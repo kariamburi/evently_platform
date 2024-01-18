1783e2c0c90ccbc63c72dc1d16091c1ad3131c85
Paul Irungu <paul.irungu@gmail.com>
2024-01-18 13:17:38 +0300

    first commit

--- app/api/webhook/clerk/B2BMMFResultURL.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-  $callbackResponse = file_get_contents('php://input');
-  $logFile = "AA3Response.json";
-  $log = fopen($logFile, "a");
-  fwrite($log, $callbackResponse);
-  fclose($log);
